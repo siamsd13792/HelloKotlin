6d86da4b9aa4e1b20d9dc0a9fdf09542a76a987a
Siam.kiw <siam13792@gmail.com>
2018-05-08 01:20:40 +0700

    Hello Kotlin

---
+++ src/HelloKotlin.kt
@@ -0,0 +1,12 @@
+
+fun main(args: Array<String>) {
+    var str: String = "Hello World"
+    val age: Int = 23
+    println(str)
+
+    fun hello(x: Int): Int{
+        return x *2
+    }
+
+    println(hello(10))
+}
